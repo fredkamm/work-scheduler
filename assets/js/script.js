11f0091632e1b5adf42ef33a423421a0ab085afd
fredkamm <105788241+fredkamm@users.noreply.github.com>
2022-07-27 14:54:35 -0400

    added html and css content, js file added

---
+++ assets/js/script.js
@@ -0,0 +1,22 @@
+// GIVEN I am using a daily planner to create a schedule
+// WHEN I open the planner
+// THEN the current day is displayed at the top of the calendar
+    // how?
+        // moment()
+        //  thurs, september 5
+// WHEN I scroll down
+// THEN I am presented with timeblocks for standard business hours
+    // hard code the time blocks
+// WHEN I view the timeblocks for that day
+// THEN each timeblock is color coded to indicate whether it is in the past, present, or future
+    // css already had those values stores 
+// WHEN I click into a timeblock
+// THEN I can enter an event
+// WHEN I click the save button for that timeblock
+// THEN the text for that event is saved in local storage
+// WHEN I refresh the page
+// THEN the saved events persist
+
+// hard cope the 9-5 time blocks
+// so each row is unique and button is unique
+// add 3 bootstrap sections, adjust size of the columns 
