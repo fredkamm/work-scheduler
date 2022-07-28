af8a31ec9250974ac03d5b7ee7410d2d1081bbd0
fredkamm <105788241+fredkamm@users.noreply.github.com>
2022-07-28 16:10:38 -0400

    successfully added js code to make the columns color cordinated with the time of the hour, last thing to do is local storage

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,13 +13,14 @@
 // WHEN I refresh the page
 // THEN the saved events persist
 
-// hard cope the 9-5 time blocks
-// so each row is unique and button is unique
-// add 3 bootstrap sections, adjust size of the columns 
+    // hard code the 9-5 time blocks
+    // so each row is unique and button is unique
+    // add 3 bootstrap sections, adjust size of the columns 
 
-// assign variables
+// current time varibale 
 var currentDayEl = $('#currentDay');
 
+// button variables
 var inputBtn1 = $('#saveBtn1');
 var inputBtn2 = $('#saveBtn2');
 var inputBtn3 = $('#saveBtn3');
@@ -30,21 +31,43 @@ var inputBtn7 = $('#saveBtn7');
 var inputBtn8 = $('#saveBtn8');
 var inputBtn9 = $('#saveBtn9');
 
-var workListEl1 = $('#workList1');
-var workListEl2 = $('#workList2');
-var workListEl3 = $('#workList3');
-var workListEl4 = $('#workList4');
-var workListEl5 = $('#workList5');
-var workListEl6 = $('#workList6');
-var workListEl7 = $('#workList7');
-var workListEl8 = $('#workList8');
-var workListEl9 = $('#workList9');
+// textarea variables
+var workListEl1 = $('#hour-9');
+var workListEl2 = $('#hour-10');
+var workListEl3 = $('#hour-11');
+var workListEl4 = $('#hour-12');
+var workListEl5 = $('#hour-1');
+var workListEl6 = $('#hour-2');
+var workListEl7 = $('#hour-3');
+var workListEl8 = $('#hour-4');
+var workListEl9 = $('#hour-5');
 
-var allWorkList = [workListEl1, workListEl2, workListEl3, workListEl4, workListEl5, workListEl6, workListEl7, workListEl8, workListEl9]; 
+// each row variable
+var hour9 = $('#rowOne');
+var hour10 = $('#rowTwo');
+var hour11 = $('#rowThree');
+var hour12 = $('#rowFour');
+var hour13 = $('#rowFive');
+var hour14 = $('#rowSix');
+var hour15 = $('#rowSeven');
+var hour16 = $('#rowEight');
+var hour17 = $('#rowNine');
 
+// assigning each row a number that represents the time in miltary time
+hour9 = 9;
+hour10 = 10;
+hour11 = 11;
+hour12 = 12;
+hour13 = 13;
+hour14 = 14;
+hour15 = 15;
+hour16 = 16;
+hour17 = 17;
 
+// variable that calculates the time now in hours
+var hour = moment().format('H');
 
-// console.log(allWorkList);
+console.log(hour);
 
 // displaying the current date and day of the week
 function displayTime() {
@@ -54,19 +77,145 @@ function displayTime() {
 }
 displayTime();
 
-// function to saving the inputs to local storage
-function saveInput(){
-    localStorage.setItem('<textarea>', workListEl1);
+// next 9 functions are what makes the column change color with time
+// im sure theres an easier/cleaner way to loop this but its working as i want.
+function past9(){
+    if (hour9 < hour) {
+        workListEl1.attr("style", 'background-color: lightgrey');
+    }
+    if (hour9 == hour) {
+        workListEl1.attr("style", 'background-color:  #ff6961');
+    }
+    if (hour9 > hour) {
+        workListEl1.attr("style", 'background-color: lightgreen');
+    }
+    
 }
+past9();
 
-console.log(saveInput);
-
-
-function saveBtnOne (){
-    inputBtn1.addEventListener('click', saveInput);
+function past10(){
+    if (hour10 < hour) {
+        workListEl2.attr("style", 'background-color: lightgrey');
+    }
+    if (hour10 == hour) {
+        workListEl2.attr("style", 'background-color:  #ff6961');
+    }
+    if (hour10 > hour) {
+        workListEl2.attr("style", 'background-color: lightgreen');
+    }
+    
 }
+past10();
+
+function past11(){
+    if (hour11 < hour) {
+        workListEl3.attr("style", 'background-color: lightgrey');
+    }
+    if (hour11 == hour) {
+        workListEl3.attr("style", 'background-color:  #ff6961');
+    }
+    if (hour11 > hour) {
+        workListEl3.attr("style", 'background-color: lightgreen');
+    }
+    
+}
+past11();
+
+function past12(){
+    if (hour12 < hour) {
+        workListEl4.attr("style", 'background-color: lightgrey');
+    }
+    if (hour12 == hour) {
+        workListEl4.attr("style", 'background-color:  #ff6961');
+    }
+    if (hour12 > hour) {
+        workListEl4.attr("style", 'background-color: lightgreen');
+    }
+    
+}
+past12();
+
+function past13(){
+    if (hour13 < hour) {
+        workListEl5.attr("style", 'background-color: lightgrey');
+    }
+    if (hour13 == hour) {
+        workListEl5.attr("style", 'background-color:  #ff6961');
+    }
+    if (hour13 > hour) {
+        workListEl5.attr("style", 'background-color: lightgreen');
+    }
+    
+}
+past13();
+
+function past14(){
+    if (hour14 < hour) {
+        workListEl6.attr("style", 'background-color: lightgrey');
+    }
+    if (hour14 == hour) {
+        workListEl6.attr("style", 'background-color:  #ff6961');
+    }
+    if (hour14 > hour) {
+        workListEl6.attr("style", 'background-color: lightgreen');
+    }
+    
+}
+past14();
+
+function past15(){
+    if (hour15 < hour) {
+        workListEl7.attr("style", 'background-color: lightgrey');
+    }
+    if (hour15 == hour) {
+        workListEl7.attr("style", 'background-color:  #ff6961');
+    }
+    if (hour15 > hour) {
+        workListEl7.attr("style", 'background-color: lightgreen');
+    }
+    
+}
+past15();
+
+function past16(){
+    if (hour16 < hour) {
+        workListEl8.attr("style", 'background-color: lightgrey');
+    }
+    if (hour16 == hour) {
+        workListEl8.attr("style", 'background-color: #ff6961');
+    }
+    if (hour16 > hour) {
+        workListEl8.attr("style", 'background-color: lightgreen');
+    } 
+}
+past16();
+
+function past17(){
+    if (hour17 < hour) {
+        workListEl9.attr("style", 'background-color: lightgrey');
+    }
+    if (hour17 == hour) {
+        workListEl9.attr("style", 'background-color:  #ff6961');
+    }
+    if (hour17 > hour) {
+        workListEl9.attr("style", 'background-color: lightgreen');
+    }
+}
+past17();
 
 
-// for (var i = 0; i < allWorkList.length; i++) {
-//     var input = allWorkList[i].value;
-//     localStorage.setItem(allWorkList[i])
+
+
+// // function to saving the inputs to local storage
+// function saveInput(){
+//     inputBtn1.addEventListener('click', function(){
+//         if(workListEl1 = "hi");
+//         window.alert("hi)");
+    
+//         localStorage.setItem('hour-9', workListEl1 )
+//     });
+// }
+//     localStorage.setItem('<textarea>', workListEl1);
+// }
+
+// console.log(saveInput)
